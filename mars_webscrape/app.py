5f05a4f11814e6a46ab88393bf28aafe444a4369
Sheri Rosalia <32849807+sherirosalia@users.noreply.github.com>
2019-01-15 09:56:49 -0800

    Did some minor tweaks to make working on localhost smooth

---
+++ mars_webscrape/app.py
@@ -0,0 +1,45 @@
+#import dependencies/libraries
+
+from flask import Flask, render_template, redirect
+#?------>
+# from flask.ext.pymongo import PyMongo 
+from flask_pymongo import PyMongo
+import scrape_mars
+import os
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+app.config['MONGO_DBNAME'] = 'mytestdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mytestdb'
+mongo = PyMongo(app)
+print(mongo.db.mars.find_one())
+
+
+@app.route('/')
+
+def index():
+	# Find one record of data from the mongo database
+    mars = mongo.db.mars.find_one()
+    #print(mars)
+    return render_template('index.html', mars=mars)
+
+ 
+# Route that will trigger the scrape function
+@app.route('/scrape')
+
+def scrape():
+    mars = mongo.db.mars
+    #mongo.db.mars.update({"id": 1}, {"$set: mars_info"}, upsert = True)
+    #mars = mongo.db.mars
+    mars_info = scrape_mars.scrape()
+    mars.update({}, mars_info, upsert=True)
+    
+    return redirect("/", code=302)
+    #return redirect("http://localhost:5000/", code=302)
+    #return redirect("http://127.0.0.1:5000/")
+
+if __name__ == '__main__':
+    app.run()
+    #app.run(debug=True)
